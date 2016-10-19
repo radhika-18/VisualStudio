e056240d8df314d95b60ce98bf7719c1cc248268
nair_r <nairradhika18@gmail.com>
2016-10-19 16:30:46 +0530

    code

---
+++ Interfaces_abstract/Interface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    /// <summary>
+    /// Interface that contains basic attributes and details of vehicle
+    /// </summary>
+    interface IVehicle
+    {
+        string Color { get; set; }
+
+        int Capacity { get; set; }
+
+        void Accelerate(); //not decided the return type
+
+        void Brake(); //return type
+    }
+
+    /// <summary>
+    /// Interface to activate ABS
+    /// </summary>
+    interface ISafety
+    {
+        bool operateABS();
+    }
+}
---
+++ Interfaces_abstract/Vehicle_Type.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface;
+
+namespace Interfaces_abstract
+{
+    class TwoWheeler : IVehicle
+    {
+        public int Capacity
+        {
+            get;
+            set;
+        }
+
+        public string Color
+        {
+            get;
+            set;
+        }
+
+        public void Accelerate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Brake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class FourWheeler : IVehicle
+    {
+        public int Capacity
+        {
+            get;
+            set;
+        }
+
+        public string Color
+        {
+            get;
+            set;
+        }
+
+        public void Accelerate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Brake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class SportsBike : TwoWheeler, ISafety
+    {
+        public bool operateABS()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class RacingCar : FourWheeler, ISafety
+    {
+        public bool operateABS()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
